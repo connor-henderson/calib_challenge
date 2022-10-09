399cc9da8a9f025d12cc9dc44de89670b2fe264e
Connor Henderson <connor.henderson@talkiatry.com>
2022-10-09 12:54:16 -0400

    added fns to handle determining pitch, yaw from vp

--- data_loader.py
+++ data_loader.py
@@ -37,3 +37,22 @@ def get_and_save_test_data():
 get_and_save_training_data()
 get_and_save_test_data()
 
+
+
+
+# "To rotate from one frame into another with euler angles the convention is to 
+# rotate around roll, then pitch and then yaw, while rotating around the rotated 
+# axes, not the original axes."
+
+# ecef = earth centered earth fixed
+
+# Test to add for going from vp -> _roll, pitch, yaw: make the vp the center of the camera and see if the MSE is 100%
+# (because 0 roll, pitch, and yaw) would be the same as the camera being mounted perfectly
+
+
+# ecef_from_local = rot_from_quat(quats_ecef[0])
+# local_from_ecef = ecef_from_local.T
+# positions_local = np.einsum('ij,kj->ki', local_from_ecef, postions_ecef - positions_ecef[0])
+# rotations_global = rot_from_quat(quats_ecef)
+# rotations_local = np.einsum('ij,kjl->kil', local_from_ecef, rotations_global)
+# eulers_local = euler_from_rot(rotations_local)
