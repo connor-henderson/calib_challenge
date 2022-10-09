3f1cf39afbe66bb710cfa81a8f0c45ac58481b92
Connor Henderson <connor.henderson@talkiatry.com>
2022-10-09 19:16:49 -0400

    add gray scale conversion, gaussian noise, canny edge detector, and hough transform

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
 def get_ground_truth_labels(i):
@@ -14,8 +16,64 @@ def get_video_frames(i):
     while ret:
         ret, img = cap.read() # read one frame from the 'capture' object; img is (H, W, C)
         if ret:
-            frames.append(img)
-    return np.stack(frames, axis=0) # dimensions (T, H, W, C)
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            blurred_gray_img = cv2.GaussianBlur(gray_img,(5,5),0) # additional image smoothing
+            
+            # Masking with a 4-sided polygon
+            canny = cv2.Canny(blurred_gray_img,25,75) #minVal, maxVal chosen from trial and error + here: https://s3.us-east-2.amazonaws.com/elasticbeanstalk-us-east-2-856193192518/files/ece493/Assignment1-Lane+Detection.pdf
+            
+            
+            mask = np.zeros_like(canny)
+            ignore_mask_color = 255
+            
+            imshape = canny.shape
+            y = imshape[0]
+            x = imshape[1]
+            y_offset = -10
+            x_offset = 100
+            
+            roi_vertices = np.array([[(200, y - 200),
+                                      (x / 2 - x_offset, y / 2 + y_offset),
+                                      (x / 2 + x_offset, y / 2 + y_offset),
+                                      (x - 200, y - 200)]],
+                                      dtype=np.int32)
+            
+            # cv2.fillPoly(img, pts=[roi_vertices], color=(0, 255, 0))
+            cv2.fillPoly(mask, roi_vertices, ignore_mask_color)
+            
+            
+            masked_edges = cv2.bitwise_and(canny, mask)
+            # masked_edges_img = np.dstack((masked_edges, masked_edges, masked_edges))
+            # plt.imshow(masked_edges_img)
+            
+            
+            # plt.subplot(121),plt.imshow(blurred_gray_img,cmap = 'gray')
+            # plt.title('blurred gray Image'), plt.xticks([]), plt.yticks([])
+            # plt.subplot(122),plt.imshow(masked_edges,cmap = 'gray')
+            # plt.title('masked Image'), plt.xticks([]), plt.yticks([])
+            # plt.show()
+            # break
+            
+            rho = 1
+            theta = 31 * np.pi/180
+            threshold = 10
+            min_line_length = 5
+            max_line_gap = 20
+            lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+            
+            line_image = np.copy(img) * 0
+            for line in lines:
+                for x0, y0, x1, y1 in line:
+                    cv2.line(line_image, (x0, y0), (x1, y1), (0, 0, 255), 4)
+            
+            plt.subplot(121),plt.imshow(line_image,cmap = 'gray')
+            plt.title('Line Image'), plt.xticks([]), plt.yticks([])
+            plt.show()
+            
+            frames.append(masked_edges)
+            break
+    return
+    return np.stack(frames, axis=0) # dimensions (T, H, W)
 
 def get_and_save_training_data():
     X = []
@@ -34,8 +92,12 @@ def get_and_save_test_data():
     np.save('data/test.npy', test_data)
     
 
-get_and_save_training_data()
-get_and_save_test_data()
+# get_and_save_training_data()
+# get_and_save_test_data()
+
+for i in range(0, 3):
+    get_video_frames(i)
+
 
 
 
@@ -46,8 +108,6 @@ get_and_save_test_data()
 
 # ecef = earth centered earth fixed
 
-# Test to add for going from vp -> _roll, pitch, yaw: make the vp the center of the camera and see if the MSE is 100%
-# (because 0 roll, pitch, and yaw) would be the same as the camera being mounted perfectly
 
 
 # ecef_from_local = rot_from_quat(quats_ecef[0])
