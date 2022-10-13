ca876509ceca2e7f54a42030d76e5d297c55a805
Connor Henderson <connor.henderson@talkiatry.com>
2022-10-12 22:09:18 -0400

    add video player

--- video-player.py
+++ video-player.py
@@ -1,10 +1,26 @@
 import cv2
 import numpy as np
+from process import process_image
 
-training_data = np.load('data/training.npy', allow_pickle=True) # shape: (frame, calibration)
+# This is the dumb way to do it:
+# training_data = np.load('data/training.npy', allow_pickle=True) # shape: (frame, calibration)
 
-for i in range(100):
-    vp = (0, 0) # debug_process_image(training_data[i][0])
-    frame = training_data[i][0]
-    cv2.imshow('img', frame)
-    cv2.waitKey(50)
+def run_video_player():
+    left_prev_lanes = []
+    right_prev_lanes = []
+    
+    cap = cv2.VideoCapture('unlabeled/9.hevc')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            frame_copy = np.copy(frame)
+            vp, img = process_image(frame_copy, left_prev_lanes, right_prev_lanes)
+            cv2.imshow('lined', img)
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+run_video_player()
